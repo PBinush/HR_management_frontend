19394af6a4ee1bbc478cf8e704afec48dc9829e0
PBinush <pasidbinush@gmail.com>
2025-04-29 14:11:13 +0530

    update model

--- src/app/model/employee.ts
+++ src/app/model/employee.ts
@@ -12,4 +12,34 @@ export class Employee{
     jobTitle!: string;
     active!: boolean;
     updateBy?: number;
+
+    constructor(
+        id: number,
+        employeeCode: string,
+        name: string,
+        email: string,
+        address: string,
+        phone: string,
+        dateOfBirth: string,
+        joinDate: string,
+        gender: 'MALE' | 'FEMALE',
+        department: string,
+        jobTitle: string,
+        active: boolean,
+        updateBy: number
+      ) {
+        this.id = id;
+        this.employeeCode = employeeCode;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+        this.dateOfBirth = dateOfBirth;
+        this.joinDate = joinDate;
+        this.gender = gender;
+        this.department = department;
+        this.jobTitle = jobTitle;
+        this.active = active;
+        this.updateBy = updateBy;
+      }
 }
