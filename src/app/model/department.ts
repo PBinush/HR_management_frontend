ef2b2b523d8f86e4a29621f4acdf7e14ae34d50f
PBinush <pasidbinush@gmail.com>
2025-04-29 15:38:55 +0530

    depatment model class implement

---
+++ src/app/model/department.ts
@@ -0,0 +1,12 @@
+export class Department {
+    id: number;
+    name: string;
+    description: string;
+  
+    constructor(id: number, name: string, description: string) {
+      this.id = id;
+      this.name = name;
+      this.description = description;
+    }
+  }
+  
